5977cf10baad31921748600ba9eaf7d1c9787123
osense <krupicka.adam@gmail.com>
2015-07-20 13:06:47 +0200

    Rewrote the implementation to use a different layout. This seems to offer about 30% speed up.

--- src/eart.erl
+++ src/eart.erl
@@ -1,61 +1,48 @@
 -module(eart).
 
 -export([new/0, insert/3, lookup/2]).
--compile([export_all]).
 
 
 %% API
 new() ->
-    {none, none, []}.
+    {none, none, empty, empty}.
 
 
-insert(Key, Value, {NodeKey, _, Children}) when NodeKey == none orelse Key == NodeKey ->
-    {Key, Value, Children};
-insert(Key, Value, {NodeKey, NodeVal, Children}) ->
+insert(Key, Value, {NodeKey, _, Fit, NoFit}) when NodeKey == none orelse Key == NodeKey ->
+    {Key, Value, Fit, NoFit};
+insert(Key = <<KHead:1/binary, _/binary>>, Value, {NodeKey = <<NHead:1/binary, _/binary>>, NodeVal, Fit, NoFit}) when KHead /= NHead ->
+    {NodeKey, NodeVal, Fit, insert(Key, Value, ensure_nonempty(NoFit))};
+insert(Key, Value, {NodeKey, NodeVal, Fit, NoFit}) ->
     PrefLen = binary:longest_common_prefix([Key, NodeKey]),
     <<Pref:PrefLen/binary, RestPref/binary>> = NodeKey,
     <<Pref:PrefLen/binary, RestKey/binary>> = Key,
     if
         RestPref == <<>> ->
-            {NodeKey, NodeVal, insert_into_children(RestKey, Value, Children, [])};
-        (RestPref /= <<>>) and (RestKey /= <<>>) ->
-            {Pref, none, [insert(RestKey, Value, new()), {RestPref, NodeVal, Children}]};
-        (RestPref /= <<>>) and (RestKey == <<>>) ->
-            {Pref, Value, [{RestPref, NodeVal, Children}]}
-    end.
-
-insert_into_children(Key, Value, [], AccOut) ->
-    [insert(Key, Value, new()) | AccOut];
-insert_into_children(Key, Value, [Child = {ChildKey, _, _} | Rest], AccOut) ->
-    PrefLen = binary:longest_common_prefix([Key, ChildKey]),
-    <<Pref:PrefLen/binary, _Rest/binary>> = ChildKey,
-    if
-        Pref == <<>> ->
-            insert_into_children(Key, Value, Rest, [Child | AccOut]);
-        Pref /= <<>> ->
-            Rest ++ [insert(Key, Value, Child) | AccOut]
-    end.
-
-
-lookup(Key, {Key, Value, _}) ->
-    Value;
-lookup(Key, {NodeKey, _NodeVal, Children}) ->
-    PrefLen = binary:longest_common_prefix([Key, NodeKey]),
-    <<_Pref:PrefLen/binary, RestPref/binary>> = NodeKey,
-    if
-        RestPref == <<>> ->
-            <<_Pref:PrefLen/binary, RestKey/binary>> = Key,
-            lookup_in_children(RestKey, Children);
+            {NodeKey, NodeVal, insert(RestKey, Value, ensure_nonempty(Fit)), NoFit};
         RestPref /= <<>> ->
-            none
+            if
+                RestKey /= <<>> ->
+                    SplitFit = {RestPref, NodeVal, Fit, NoFit},
+                    {Pref, none, insert(RestKey, Value, SplitFit), empty};
+                RestKey == <<>> ->
+                    {Key, Value, {RestPref, NodeVal, Fit, empty}, NoFit}
+            end
     end.
 
-lookup_in_children(_Key, []) ->
-    none;
-lookup_in_children(Key, [Child | Rest]) ->
-    case lookup(Key, Child) of
-        none ->
-            lookup_in_children(Key, Rest);
-        Val ->
-            Val
-    end.
+ensure_nonempty(empty) ->
+    new();
+ensure_nonempty(T) ->
+    T.
+
+lookup(Key, {Key, Value, _, _}) ->
+    Value;
+lookup(Key, {NodeKey, _NodeVal, Fit, NoFit}) ->
+    PrefLen = binary:longest_common_prefix([Key, NodeKey]),
+    <<Pref:PrefLen/binary, RestKey/binary>> = Key,
+    case Pref of
+        NodeKey -> lookup(RestKey, Fit);
+        <<>> -> lookup(Key, NoFit);
+        _ -> none
+    end;
+lookup(_Key, empty) ->
+    none.
--- test/eart_tests.erl
+++ test/eart_tests.erl
@@ -7,25 +7,25 @@
 insert_test() ->
     T1 = eart:new(),
     T2 = eart:insert(<<"roman">>, 100, T1),
-    ?assertEqual({<<"roman">>, 100, []}, T2),
+    ?assertEqual({<<"roman">>, 100, empty, empty}, T2),
     T3 = eart:insert(<<"romanus">>, val2, T2),
     ?assertEqual({<<"roman">>, 100,
-        [{<<"us">>, val2, []}]},
+        {<<"us">>, val2, empty, empty},
+        empty},
     T3),
     T4 = eart:insert(<<"rome">>, val3, T3),
-    ?assertEqual({<<"rom">>, none, [
-        {<<"e">>, val3, []},
+    ?assertEqual({<<"rom">>, none,
         {<<"an">>, 100,
-            [{<<"us">>, val2, []}]}]},
+            {<<"us">>, val2, empty, empty},
+            {<<"e">>, val3, empty, empty}},
+        empty},
     T4),
     T5 = eart:insert(<<"aaa">>, val4, T4),
-    ?assertEqual({<<"">>, none, [
-        {<<"aaa">>, val4, []},
-        {<<"rom">>, none, [
-            {<<"e">>, val3, []},
-            {<<"an">>, 100,
-                [{<<"us">>, val2, []}]}]}
-        ]},
+    ?assertEqual({<<"rom">>, none,
+        {<<"an">>, 100,
+            {<<"us">>, val2, empty, empty},
+            {<<"e">>, val3, empty, empty}},
+        {<<"aaa">>, val4, empty, empty}},
     T5).
 
 lookup_test() ->
@@ -35,7 +35,8 @@ lookup_test() ->
     T4 = eart:insert(<<"rome">>, val3, T3),
     ?assertEqual(100, eart:lookup(<<"roman">>, T4)),
     ?assertEqual(val2, eart:lookup(<<"romanus">>, T4)),
-    ?assertEqual(val3, eart:lookup(<<"rome">>, T4)).
+    ?assertEqual(val3, eart:lookup(<<"rome">>, T4)),
+    ?assertEqual(none, eart:lookup(<<"noo">>, T4)).
 
 combined_test() ->
     T1 = eart:new(),
